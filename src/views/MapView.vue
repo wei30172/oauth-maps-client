d5b8c78bf7f0fd856b8786ecb91cccda937e9246
claireWei <claire610323@gmail.com>
2022-09-06 22:36:46 +0800

    map basic ui

--- src/views/MapView.vue
+++ src/views/MapView.vue
@@ -1,10 +1,35 @@
 <template>
-  <div class="map">Map</div>
+  <div class="map">
+    <h1>Map</h1>
+    <div v-if="authIsReady">
+      <div v-if="!user.fbAvatar">
+        <router-link to="/profile">
+          Please bind fb to see more content.
+        </router-link>
+      </div>
+      <div v-else><p>Search</p></div>
+    </div>
+  </div>
 </template>
 
 <script>
 export default {
-  name: "MapView",
-  components: {},
+  setup() {
+    return {
+      user: {
+        email: "test@gmail.com",
+      },
+      authIsReady: true,
+    };
+  },
 };
 </script>
+
+<style lang="scss" scoped>
+@use "../styles/mixin/colors" as colors;
+.map {
+  a {
+    color: colors.$text-primary;
+  }
+}
+</style>
