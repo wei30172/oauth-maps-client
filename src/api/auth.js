09e6f5cb48ebb216bdedad847753f9551da7eac5
claireWei <claire610323@gmail.com>
2022-09-08 02:52:34 +0800

    deploy maps app

--- src/api/auth.js
+++ src/api/auth.js
@@ -2,18 +2,18 @@ import { axiosPrivate } from "./axios";
 
 // get user
 export const getLoginUser = async () => {
-  const { data } = await axiosPrivate.get(`/auth/user`);
+  const { data } = await axiosPrivate.get(`auth/user`);
   return data.user;
 };
 
 // get fb info
 export const getFBInfo = async () => {
-  const { data } = await axiosPrivate.get(`/auth/userfb`);
+  const { data } = await axiosPrivate.get(`auth/userfb`);
   return data.fbinfo;
 };
 
 // user logout
 export const userLogout = async () => {
-  await axiosPrivate.get(`/auth/logout`);
+  await axiosPrivate.get(`auth/logout`);
   return;
 };
--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,13 @@
 const { defineConfig } = require("@vue/cli-service");
+const path = require("path");
 module.exports = defineConfig({
   transpileDependencies: true,
+  outputDir: path.resolve(__dirname, "../oauth-maps-server/public"),
+  devServer: {
+    proxy: {
+      "/auth": {
+        target: "http://localhost:3000",
+      },
+    },
+  },
 });
