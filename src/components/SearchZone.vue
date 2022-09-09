35cc49fe0331b37ce55901bd046e08d56afb2ebb
claireWei <claire610323@gmail.com>
2022-09-09 16:05:12 +0800

    add default map and search function on map page

---
+++ src/components/SearchZone.vue
@@ -0,0 +1,65 @@
+<template>
+  <div class="searchzone">
+    <form @submit.prevent="$emit('handleClick', queryIp)" class="flex">
+      <input
+        class="search-input"
+        type="text"
+        placeholder="Enter your address or leave it empty to get your current position."
+        v-model="queryIp"
+      />
+      <div
+        class="search-icon cursor-pointer"
+        @click="$emit('handleClick', queryIp)"
+      >
+        <SearchIcon />
+      </div>
+    </form>
+  </div>
+</template>
+
+<script>
+import { ref } from "vue";
+import SearchIcon from "@/features/Icons/SearchIcon.vue";
+
+export default {
+  components: { SearchIcon },
+  setup() {
+    const queryIp = ref("");
+    return { queryIp };
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+@use "../styles/mixin/_colors.scss" as colors;
+.searchzone {
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  position: relative;
+  padding: 1rem;
+  background-color: colors.$background-dark;
+  form {
+    width: 100%;
+  }
+  .search-input {
+    width: 100%;
+    max-width: 760px;
+    display: flex;
+    flex: 1 1 0%;
+    padding: 1rem;
+    background-color: colors.$white;
+    border-top-left-radius: 6px;
+    border-bottom-left-radius: 6px;
+  }
+  .search-icon {
+    background-color: colors.$primary;
+    color: colors.$white;
+    width: 3rem;
+    height: 3rem;
+    border-top-right-radius: 6px;
+    border-bottom-right-radius: 6px;
+  }
+}
+</style>
--- src/views/MapView.vue
+++ src/views/MapView.vue
@@ -1,22 +1,91 @@
 <template>
   <div class="map flex">
-    <h1>Map</h1>
-    <router-link to="/profile">
-      Please bind fb to see more content.
+    <!-- bind Facebook link -->
+    <router-link v-if="!fbUserID" to="/profile">
+      Go to the profile page to bind Facebook to see more content.
     </router-link>
+
+    <!-- tracker -->
+    <section class="tracker">
+      <!-- Map -->
+      <div id="map"></div>
+      <!-- Search -->
+      <SearchZone v-if="fbUserID" @handleClick="getIpInfo" />
+      <!-- IP Info -->
+      <IPInfo v-if="ipInfo" :ipInfo="ipInfo" />
+    </section>
   </div>
 </template>
 
 <script>
-import { computed } from "vue";
+import leaflet from "leaflet";
+import axios from "axios";
+import { ref, computed, onMounted } from "vue";
 import { useStore } from "vuex";
+import { getFBInfo } from "@/api/auth";
+import SearchZone from "@/components/SearchZone";
+import IPInfo from "@/components/IPInfo";
 
 export default {
+  components: { SearchZone, IPInfo },
   setup() {
     const store = useStore();
+    const fbUserID = ref(null);
+    let map;
+    let ipInfo = ref(null);
+
+    const getId = async () => {
+      const info = await getFBInfo();
+      if (info) fbUserID.value = info.id;
+    };
+    getId();
+
+    // creates the map
+    onMounted(() => {
+      const maptilerurl =
+        "https://api.maptiler.com/maps/streets/{z}/{x}/{y}.png?key=3M3Hd2D6IxuQIX5M1hu1";
+
+      const attribution =
+        '\u003ca href="https://www.maptiler.com/copyright/" target="_blank"\u003e\u0026copy; MapTiler\u003c/a\u003e \u003ca href="https://www.openstreetmap.org/copyright" target="_blank"\u003e\u0026copy; OpenStreetMap contributors\u003c/a\u003e';
+
+      map = leaflet.map("map").setView([23.6978, 120.9605], 5);
+      leaflet
+        .tileLayer(maptilerurl, {
+          tileSize: 512,
+          zoomOffset: -1,
+          minZoom: 1,
+          attribution: attribution,
+          crossOrigin: true,
+        })
+        .addTo(map);
+    });
+
+    // gets ip information from API
+    const getIpInfo = async (queryIp) => {
+      try {
+        const geourl = `https://geo.ipify.org/api/v1?apiKey=${process.env.VUE_APP_MAPS_APIKEY}&ipAddress=${queryIp}`;
+
+        const { data } = await axios.get(geourl);
+        ipInfo.value = {
+          address: data.ip,
+          state: data.location.region,
+          timezone: data.location.timezone,
+          isp: data.isp,
+          lat: data.location.lat,
+          lng: data.location.lng,
+        };
+        leaflet.marker([ipInfo.value.lat, ipInfo.value.lng]).addTo(map);
+        map.setView([ipInfo.value.lat, ipInfo.value.lng], 13);
+      } catch (err) {
+        alert(err.message);
+      }
+    };
 
     return {
       user: computed(() => store.state.user),
+      fbUserID,
+      ipInfo,
+      getIpInfo,
     };
   },
 };
@@ -26,11 +95,25 @@ export default {
 @use "../styles/mixin/colors" as colors;
 .map {
   flex-direction: column;
-  gap: 2rem;
+  gap: 0.5rem;
   a {
     color: colors.$text-primary;
     &:hover {
-      border-bottom: 2px solid colors.$text-primary;
+      color: colors.$facebook-primary;
+    }
+  }
+  .tracker {
+    position: relative;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    width: 100%;
+    height: 85vh;
+    #map {
+      margin-top: 1rem;
+      width: 100%;
+      height: 100%;
+      z-index: 10;
     }
   }
 }
