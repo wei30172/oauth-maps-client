2ccd52ac78910844e1336a5169d60fce06ef4196
claireWei <claire610323@gmail.com>
2022-09-10 21:21:16 +0800

    add loading modal

---
+++ src/components/LoadingModal.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="loading-modal page-flex">
+    <LoadingIcon class="loading_icon" />
+    <h1>Loading...</h1>
+  </div>
+</template>
+
+<script>
+import LoadingIcon from "@/features/Icons/LoadingIcon.vue";
+
+export default {
+  components: { LoadingIcon },
+
+  props: ["type"],
+};
+</script>
+
+<style lang="scss" scoped>
+@use "../styles/mixin/colors" as colors;
+.loading-modal {
+  flex-direction: column;
+  z-index: 99;
+  background-color: #16161695;
+  position: absolute;
+  width: 100%;
+  height: 100vh;
+  color: colors.$white;
+  .loading_icon {
+    width: 20%;
+    height: 20%;
+  }
+}
+</style>
