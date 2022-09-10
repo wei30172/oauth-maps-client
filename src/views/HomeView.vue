4077542dc7c17fff0d633db003de62f5f142c1f6
claireWei <claire610323@gmail.com>
2022-09-10 12:41:04 +0800

    update home page

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,5 +1,10 @@
 <template>
-  <div class="home flex"><h1>Home</h1></div>
+  <div class="home flex">
+    <h1>新北市都市更新地點查詢</h1>
+    <router-link to="/login">
+      <h3>(請先登入)</h3>
+    </router-link>
+  </div>
 </template>
 
 <script>
@@ -10,7 +15,14 @@ export default {
 </script>
 
 <style lang="scss" scoped>
+@use "../styles/mixin/colors" as colors;
 .home {
   flex-direction: column;
+  a {
+    color: colors.$text-primary;
+    &:hover {
+      color: colors.$google-primary;
+    }
+  }
 }
 </style>
